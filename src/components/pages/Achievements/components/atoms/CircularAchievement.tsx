25c9b38061c1066c0a3b7e66aecbf749e2023ebf
Haniye Akbari <roya.miss30@gmail.com>
2024-11-29 18:49:18 +0330

    selecting highlighted achievement edited

--- src/components/pages/Achievements/components/atoms/CircularAchievement.tsx
+++ src/components/pages/Achievements/components/atoms/CircularAchievement.tsx
@@ -2,45 +2,50 @@ import {Avatar, Box, Typography} from '@mui/material';
 
 import AchieveCup from '@/assets/img/icons/general/achieveCup.svg';
 import {flex} from '@/utils/flexHelper';
+import {useNavigate} from 'react-router';
 
 type Props = {
   isFullWidth: boolean;
   isSelected: boolean;
   title: string;
 };
-const CircularAchievement = ({isFullWidth, isSelected, title}: Props) => (
-  <Box
-    sx={{
-      ...flex()
-        .column()
-        .gap(isFullWidth ? '0.5rem' : '0.25rem')
-        .jbetween()
-        .acenter().result,
-      bgcolor: 'transparent',
-    }}>
-    <Avatar
+const CircularAchievement = ({isFullWidth, isSelected, title}: Props) => {
+  const navigate = useNavigate();
+  return (
+    <Box
+      onClick={isFullWidth ? () => navigate('?show=highlighted') : undefined}
       sx={{
-        bgcolor: 'background.paper',
-        border: isSelected
-          ? theme => `1px solid ${theme.palette.primary.main}`
-          : 'none',
-        width: isFullWidth ? '100px' : '50px',
-        height: isFullWidth ? '100px' : '50px',
-        p: isFullWidth ? '1rem' : '0.5rem',
-        svg: {
-          width: isFullWidth ? '56px' : '30px',
-          height: isFullWidth ? '58px' : '30px',
-        },
-      }}
-      src={AchieveCup}
-    />
-    <Typography
-      variant={'h6'}
-      fontSize={isFullWidth ? '18px' : '10px'}
-      fontFamily={'Alfa Slab One'}>
-      {title}
-    </Typography>
-  </Box>
-);
+        ...flex()
+          .column()
+          .gap(isFullWidth ? '0.5rem' : '0.25rem')
+          .jbetween()
+          .acenter().result,
+        bgcolor: 'transparent',
+      }}>
+      <Avatar
+        sx={{
+          bgcolor: 'background.paper',
+          border: isSelected
+            ? theme => `1px solid ${theme.palette.primary.main}`
+            : 'none',
+          width: isFullWidth ? '100px' : '50px',
+          height: isFullWidth ? '100px' : '50px',
+          p: isFullWidth ? '1rem' : '0.5rem',
+          svg: {
+            width: isFullWidth ? '56px' : '30px',
+            height: isFullWidth ? '58px' : '30px',
+          },
+        }}
+        src={AchieveCup}
+      />
+      <Typography
+        variant={'h6'}
+        fontSize={isFullWidth ? '18px' : '10px'}
+        fontFamily={'Alfa Slab One'}>
+        {title}
+      </Typography>
+    </Box>
+  );
+};
 
 export default CircularAchievement;
